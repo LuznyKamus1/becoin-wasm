b2ad1febecc8babb2fba38d6585d20c5728c6269
rkinkk <r3kinek0@gmail.com>
2022-12-03 17:51:35 +0100

    a

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,13 @@ extern{
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}", name))
 }
+
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    let window = web_sys::window().except("no window (linux better)");
+    let document = window.document().except("no documento");
+    let body = document.body().except("no body");
+
+    document.getElementById("money").set_text_content(Some("test"))
+    greet();
+}
